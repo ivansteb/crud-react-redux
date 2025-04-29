b08d1a6402a229aeb8e7a5fb218633551e336e26
ivansteb <steblerivan@gmail.com>
2025-04-29 19:22:19 -0300

    feat: add Toaster component for user feedback and enhance syncWithDatabaseMiddleware for delete user action

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,45 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, Middleware } from "@reduxjs/toolkit";
+import { toast } from "sonner";
 import usersReducer from "./users/slice";
 // El store será donde almacenaremos el estado de la aplicación
 
-const persistenceLocalStorageMiddleware = (store) => (next) => (action) => {
-  next(action);
-  localStorage.setItem("__redux__state__", JSON.stringify(store.getState()));
-};
+const persistenceLocalStorageMiddleware: Middleware =
+  (store) => (next) => (action) => {
+    next(action);
+    localStorage.setItem("__redux__state__", JSON.stringify(store.getState()));
+  };
+
+const syncWithDatabaseMiddleware: Middleware =
+  (store) => (next) => (action) => {
+    const { type, payload } = action;
+
+    next(action);
+
+    if (type === "users/deleteUserById") {
+      fetch(`https://jsonplaceholder.typicode.com/users/${payload}`, {
+        method: "DELETE",
+      })
+        .then((res) => {
+          if (res.ok) {
+            toast.success("User has been deleted successfully");
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+          toast.error("Error deleting user");
+        });
+    }
+  };
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(persistenceLocalStorageMiddleware);
+    return getDefaultMiddleware().concat(
+      persistenceLocalStorageMiddleware,
+      syncWithDatabaseMiddleware
+    );
   },
 });
 
