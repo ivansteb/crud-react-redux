8cf50def11115f0d9393aa2a0b4c806037465fc9
ivansteb <steblerivan@gmail.com>
2025-04-29 15:07:27 -0300

    refactor store: add persistence middleware for logging state and actions

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,9 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./users/slice";
 // El store será donde almacenaremos el estado de la aplicación
 
+const persistanceLocalStorageMiddleware = (store) => (next) => (action) => {
+  console.log(store.getState());
+  console.log(action);
+  next(action);
+  console.log(store.getState());
+};
+
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(persistanceLocalStorageMiddleware);
   },
 });
 
